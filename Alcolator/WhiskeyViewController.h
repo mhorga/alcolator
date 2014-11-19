755cde85640855337f319669c3a55ea6444374f3
Marius Horga <mhorga@gmail.com>
2014-11-19 12:50:20 +0200

    Wrote a custom view controller

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,14 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Marius Horga on 11/19/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
